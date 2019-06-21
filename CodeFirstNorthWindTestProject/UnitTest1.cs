67041e7c9bfbea8cd620da6ce9fc707893a6be38
Karen Payne <paynekaren@comcast.net>
2019-06-21 14:00:55 -0700

    Stubbbed out several test

---
+++ CodeFirstNorthWindTestProject/UnitTest1.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeFirstNorthWindTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/ef/ef6/querying/local-data
+        /// </summary>
+        [TestMethod]
+        public void UsingLocalTest()
+        {
+        }
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/ef/ef6/querying/no-tracking
+        /// </summary>
+        [TestMethod]
+        public void TrackingNoTrackingTest() 
+        {
+
+        }
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/ef/ef6/querying/related-data
+        /// </summary>
+        [TestMethod]
+        public void LoadingRelatedDataTest()
+        {
+
+        }
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/ef/ef6/saving/change-tracking/property-values
+        /// </summary>
+        [TestMethod]
+        public void SettingIndividualPropertiesTest()
+        {
+
+        }
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/ef/ef6/saving/change-tracking/entity-state
+        /// </summary>
+        [TestMethod]
+        public void EntityStateTest()
+        {
+
+        } 
+    }
+}
